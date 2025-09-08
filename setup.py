e2176676911ad9a455b90e3b82d13a17c8b4e2d4
StoneT2000 <stonezt2019@gmail.com>
2025-09-08 15:24:02 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="easyhec",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     author="Stone Tao",
     homepage="https://github.com/stonet2000/easyhec",
-    summary="EasyHec is a library for fast and automatic camera extrinsic calibration",
+    description="EasyHec is a library for fast and automatic camera extrinsic calibration",
     license="BSD-3-Clause",
     url="https://github.com/stonet2000/easyhec",
     python_requires=">=3.9",
