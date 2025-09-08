b95811e2cdd707cf50f42d564882d85aa9adf44a
StoneT2000 <stonezt2019@gmail.com>
2025-09-08 15:11:15 -0700

    warnings about missing pkgs that need to be manually installed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="easyhec",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     author="Stone Tao",
     install_requires=[
